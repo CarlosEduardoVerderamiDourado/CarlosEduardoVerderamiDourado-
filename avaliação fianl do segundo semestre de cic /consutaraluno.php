053cc65dae4ddc17a0d25992802d50a8a608e437
CarlosEduardoVerderamiDourado <151954224+CarlosEduardoVerderamiDourado@users.noreply.github.com>
2023-12-16 14:19:45 -0300

    Create consutaraluno.php

---
+++ avaliação fianl do segundo semestre de cic /consutaraluno.php
@@ -0,0 +1,18 @@
+<!DOCTYPEhtml>
+<html lang="pt">
+<cabeça>
+    <meta charset="UTF-8">
+    <meta name="janela de visualização" content="largura=largura do dispositivo, escala inicial=1,0">
+    <title>Documento</title>
+</head>
+<corpo>
+    <form action="../conexão/listadealunosnaoalterar.php" método="post">
+        <conjunto de campos>
+        coloque o cpf do aluno:
+        <tipo de entrada="número" nome="cxcpf2" class="cxcpf2"/><br/>
+        <tipo de entrada="enviar" value="buscar ">
+        </fieldset>
+    </formulário>
+</corpo>
+</html>
+    
