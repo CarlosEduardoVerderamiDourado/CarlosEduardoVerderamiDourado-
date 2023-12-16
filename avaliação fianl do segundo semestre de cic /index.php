f02e27a6ab35bb12905e56606870639d3ecdd904
CarlosEduardoVerderamiDourado <151954224+CarlosEduardoVerderamiDourado@users.noreply.github.com>
2023-12-16 14:18:05 -0300

    Create index.php

---
+++ avaliação fianl do segundo semestre de cic /index.php
@@ -0,0 +1,23 @@
+<!DOCTYPEhtml>
+<html lang="pt-br">
+<cabeça>
+    <meta charset="UTF-8">
+    <meta name="janela de visualização" content="largura=largura do dispositivo, escala inicial=1,0">
+    <link rel="folha de estilo"
+    type="texto/css"
+    href="css/estilo.css"/>
+    <title>cadastro Autoescola</title>
+</head>
+<corpo>
+    <seção id="principal">
+    <conjunto de campos>
+        <id de navegação="menu">
+            <a href="php/orçamento.php" método = postagem>
+               cadastro </br>
+            <a href="php/consutaraluno.php">
+            consultar cadastrados
+        </nav>
+    </fieldset>
+    </seção>
+</corpo>
+</html>
